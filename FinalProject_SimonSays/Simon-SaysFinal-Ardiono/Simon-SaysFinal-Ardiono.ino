37723598516576410ca25fde8955fb99ae6d429b
Steven Taveras <dreamsniper@users.noreply.github.com>
2024-05-20 19:39:10 -0400

    updated code to create a colored pattern on the screen according to button press

--- FinalProject_SimonSays/Simon-SaysFinal-Ardiono/Simon-SaysFinal-Ardiono.ino
+++ FinalProject_SimonSays/Simon-SaysFinal-Ardiono/Simon-SaysFinal-Ardiono.ino
@@ -1,32 +1,30 @@
 int PinButt[] = {2, 3, 4, 5}; // button pins
-int PinLED[] = {8, 9, 10, 11}; //  LED pins
+int PinLED[] = {8, 9, 10, 11}; // LED pins
 int StateButt[4]; // button states
 
 void setup() {
-  Serial.begin(115200); // serial communicate
+  Serial.begin(115200); // Serial communication
   
-  // buttons as inputs with pull-up resistors and LEDs
+  // Buttons as inputs with pull-up resistors and LEDs
   for (int i = 0; i < 4; i++) {
     pinMode(PinButt[i], INPUT_PULLUP);
     pinMode(PinLED[i], OUTPUT);
-    digitalWrite(PinLED[i], LOW); // Turn off LEDs initially
+    //digitalWrite(PinLED[i], LOW); // Turn on corresponding LED
   }
-  
 }
 
 void loop() {
-  // Read button states and send to Processing
+  // Read button states and send to Processing when pressed
   for (int i = 0; i < 4; i++) {
     StateButt[i] = digitalRead(PinButt[i]);
 
     if (StateButt[i] == LOW) { // Button is pressed
       digitalWrite(PinLED[i], HIGH); // Turn on corresponding LED
-      //analogWrite(PinLED[i + 6], HIGH); // Full brightness
       Serial.write(i); // Send index to Processing
     } else { // Button is released
       digitalWrite(PinLED[i], LOW); // Turn off corresponding LED
     }
   }
   
-  delay(100); // Might use millis
+  delay(200); // Might use millis
 }
